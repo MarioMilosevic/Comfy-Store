6a1fbde156fc3c17e9deacb0584812b66dc1ba1c
Mario Milosevic <128989997+MarioMilosevic@users.noreply.github.com>
2024-03-07 16:20:01 +0100

    Add SignIn component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
-import "./App.css";
+// https://strapi-store-server.onrender.com/api/products?search=&category=all&company=all&order=a-z&price=100000
+import SignIn from "./components/SignIn";
 
 function App() {
-  return <>Mario</>;
+  return (
+    <>
+      <SignIn />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/SignIn.tsx
@@ -0,0 +1,12 @@
+const SignIn = () => {
+  return (
+    <header className="w-screen bg-indigo-950 text-indigo-50 p-2">
+      <div className="w-[56%] mx-auto flex justify-end gap-4 text-xs">
+        <button>Sign In / Guest</button>
+        <button>Create Account</button>
+      </div>
+    </header>
+  )
+}
+
+export default SignIn
